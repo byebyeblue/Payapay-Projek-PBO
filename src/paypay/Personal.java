c932fc9de39e47dc7b91023396db0694649b28df
apriladwiutami <62372893+apriladwiutami@users.noreply.github.com>
2021-01-20 11:24:43 +0700

    Create Personal.java

---
+++ src/paypay/Personal.java
@@ -0,0 +1,11 @@
+package paypay;
+
+import java.util.ArrayList;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Personal extends Akun {
+    DoubleProperty KTP;
+    StringProperty tgl_lahir;
