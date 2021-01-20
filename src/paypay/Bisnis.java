8bb07a31688c8641af417ad5a48388942c1d2fb7
nurayu1039 <62587005+nurayu1039@users.noreply.github.com>
2021-01-20 11:27:35 +0700

    Create Bisnis.java

---
+++ src/paypay/Bisnis.java
@@ -0,0 +1,9 @@
+package paypay;
+
+import java.util.ArrayList;
+import javafx.beans.property.StringProperty;
+
+public class Bisnis extends Akun {
+    StringProperty telp_bisnis;
+    StringProperty nama_bisnis;
+    StringProperty mata_uang;
