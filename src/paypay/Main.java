9ae912ac27dabe4f7a46d7fdb731d89c2d18dfa2
nurayu1039 <62587005+nurayu1039@users.noreply.github.com>
2021-01-20 11:36:40 +0700

    Create Main.java

---
+++ src/paypay/Main.java
@@ -0,0 +1,11 @@
+package paypay;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import paypay.db.DBHelper;
