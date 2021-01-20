84ce94f096e019acc09ac939491795d18062a8e8
apriladwiutami <62372893+apriladwiutami@users.noreply.github.com>
2021-01-20 21:38:42 +0700

    Update PaypayController.java

--- src/paypay/PaypayController.java
+++ src/paypay/PaypayController.java
@@ -97,3 +97,105 @@ public class PaypayController implements Initializable {
     
     @FXML
     private TableView<Card> tblCard;
+
+    @FXML
+    private TableColumn<Card, Double> colCCN;
+
+    @FXML
+    private TableColumn<Card, String> colJenis_kartu;
+
+    @FXML
+    private TableColumn<Card, String> colExp_date;
+
+    @FXML
+    private TableColumn<Card, String> colAlamat_penagihan;
+
+    @FXML
+    private TableColumn<Card, Integer> colSec_num;
+
+    @FXML
+    private TextField tfNewID;
+
+    @FXML
+    private TextField tfNewCCN;
+
+    @FXML
+    private TextField tfNewJenis_kartu;
+
+    @FXML
+    private TextField tfNewExp_date;
+
+    @FXML
+    private TextField tfNewAlamat_penagihan;
+
+    @FXML
+    private TextField tfNewSec_num;
+
+    @FXML
+    private Button btnAddCard;
+
+    @FXML
+    private Label lblDBStatus;
+    
+    @FXML
+    private Label lblSaveStatus;
+    private PaypayDataModel ahdm;
+    
+    @FXML    
+    void handleAddAccountButton(ActionEvent event) {
+        LocalDate ld = dpTgl_lahir.getValue();
+        String Tgl_lahir = String.format("%d-%02d-%02d", ld.getYear(), 
+                ld.getMonthValue(), ld.getDayOfMonth());
+        LocalDate xd = dpExp_date.getValue();
+        String Exp_date = String.format("%d-%02d-%02d", xd.getYear(), 
+                xd.getMonthValue(), xd.getDayOfMonth());
+        Personal holder = new Personal(Integer.parseInt(tfID.getText()), 
+                tfName.getText(), tfAddress.getText(), tfEmail.getText(), 
+                tfNo_hp.getText(), Double.parseDouble(tfSaldo.getText()), 
+                Double.parseDouble(tfKTP.getText()), Tgl_lahir, 
+                new Card(Double.parseDouble(tfCCN.getText()),
+                tfJenis_kartu.getText(), Exp_date, tfAlamat_penagihan.getText(),
+                Integer.parseInt(tfSec_num.getText())));
+        
+        try {
+            ahdm.addAkun(holder);
+            lblSaveStatus.setText("Akun Berhasil Dibuat");
+            btnReload.fire();
+            btnClear.fire();
+        } catch (SQLException ex) {
+            lblSaveStatus.setText("Akun Gagal Dibuat");
+            Logger.getLogger(PaypayController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    void handleAddCardButton(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void handleClearButton(ActionEvent event) {
+        try {
+            tfID.setText(""+ahdm.nextID());
+            tfName.setText("");
+	    tfAddress.setText("");
+	    tfEmail.setText("");
+	    tfNo_hp.setText("");
+	    tfKTP.setText("");
+	    tfSaldo.setText("");
+	    tfCCN.setText("");
+	    tfJenis_kartu.setText("");
+	    tfAlamat_penagihan.setText("");
+	    tfSec_num.setText("");
+            
+            dpTgl_lahir.setValue(LocalDate.of(LocalDate.now().getYear() - 20, 
+                    LocalDate.now().getMonthValue(), 
+                    LocalDate.now().getDayOfMonth()));
+            dpExp_date.setValue(LocalDate.of(LocalDate.now().getYear()+5, 
+                    LocalDate.now().getMonthValue(), 
+                    LocalDate.now().getDayOfMonth()));
+        } catch (SQLException ex) {
+            Logger.getLogger(PaypayController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	 
+    }
