959dc506569c60b5a2080339c2fcc3c836fb5373
Fan <62688472+byebyeblue@users.noreply.github.com>
2021-01-20 11:41:07 +0700

    Update PaypayDataModel.java

--- src/paypay/PaypayDataModel.java
+++ src/paypay/PaypayDataModel.java
@@ -46,3 +46,33 @@ public class PaypayDataModel {
         stmtCard.setInt(6, holder.getID());
         stmtCard.execute();
     }
+
+    public void addAkun(Bisnis holder) throws SQLException{
+        String insertAkun = "INSERT INTO akun (ID, name, address, email, no_hp, saldo)" + " VALUES (?, ?, ?, ?, ?, ?)";
+        String insertBisnis = "INSERT INTO bisnis (ID, telo_bisnis, nama_bisnis, mata_uang)" + " VALUES (?, ?, ?, ?)";
+        String insertCard = "INSERT INTO card (CCN, jenis_kartu, exp_date, alamat_penagihan, sec_num, ID)" + " VALUES (?, ?, ?, ?, ?, ?)";
+        PreparedStatement stmtAkun = conn.prepareStatement(insertAkun);
+        stmtAkun.setInt(1, holder.getID());
+        stmtAkun.setString(2, holder.getName());
+        stmtAkun.setString(3, holder.getAddress());
+        stmtAkun.setString(4, holder.getEmail());
+        stmtAkun.setString(5, holder.getNo_hp());
+        stmtAkun.setDouble(6, holder.getSaldo());
+        stmtAkun.execute();
+        
+        PreparedStatement stmtBisnis = conn.prepareStatement(insertBisnis);
+        stmtBisnis.setInt(1, holder.getID());
+        stmtBisnis.setString(2, holder.getTelp_bisnis());
+        stmtBisnis.setString(3, holder.getNama_bisnis());
+        stmtBisnis.setString(4, holder.getMata_uang());
+        stmtBisnis.execute();
+        
+        PreparedStatement stmtCard = conn.prepareStatement(insertCard);
+        stmtCard.setDouble(1, holder.getCards().get(0).getCCN());
+        stmtCard.setString(2, holder.getCards().get(0).getJenisKartu());
+        stmtCard.setString(3, holder.getCards().get(0).getExpDate());
+        stmtCard.setString(4, holder.getCards().get(0).getAlamat_penagihan());
+        stmtCard.setInt(5, holder.getCards().get(0).getSecNum());
+        stmtCard.setInt(6, holder.getID());
+        stmtCard.execute();
+    }
