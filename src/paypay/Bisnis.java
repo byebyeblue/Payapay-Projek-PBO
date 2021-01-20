6a6aaf3f19fda916f97e22133ee15c6aa2c6f4ab
apriladwiutami <62372893+apriladwiutami@users.noreply.github.com>
2021-01-20 11:28:15 +0700

    Update Bisnis.java

--- src/paypay/Bisnis.java
+++ src/paypay/Bisnis.java
@@ -7,3 +7,21 @@ public class Bisnis extends Akun {
     StringProperty telp_bisnis;
     StringProperty nama_bisnis;
     StringProperty mata_uang;
+
+    public Bisnis(Integer ID, String Name, String Address, String email, String no_hp,
+            Double saldo, String telp_bisnis, String nama_bisnis, String mata_uang,
+            ArrayList<Card> cards) {
+        super(ID, Name, Address, email, no_hp, saldo, cards);
+        this.telp_bisnis.set(telp_bisnis);
+        this.nama_bisnis.set(nama_bisnis);
+        this.mata_uang.set(mata_uang);
+    }
+
+    public Bisnis(Integer ID, String Name, String Address, String email, String no_hp, 
+            Double saldo, String telp_bisnis, String nama_bisnis, String mata_uang,
+            Card card) {
+        super(ID, Name, Address, email, no_hp, saldo, card);
+        this.telp_bisnis.set(telp_bisnis);
+        this.nama_bisnis.set(nama_bisnis);
+        this.mata_uang.set(mata_uang);
+    }
