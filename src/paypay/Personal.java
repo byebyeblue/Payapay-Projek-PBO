12564feefe345c26d1e3fbfc4fb5778fd1899fcf
nurayu1039 <62587005+nurayu1039@users.noreply.github.com>
2021-01-20 11:25:33 +0700

    Update Personal.java

--- src/paypay/Personal.java
+++ src/paypay/Personal.java
@@ -9,3 +9,17 @@ import javafx.beans.property.StringProperty;
 public class Personal extends Akun {
     DoubleProperty KTP;
     StringProperty tgl_lahir;
+
+    public Personal(Integer ID, String Name, String Address, String email, String no_hp,
+            Double saldo, Double KTP, String tgl_lahir, ArrayList<Card> cards) {
+        super(ID, Name, Address, email, no_hp, saldo, cards);
+        this.KTP = new SimpleDoubleProperty(KTP);
+        this.tgl_lahir = new SimpleStringProperty(tgl_lahir);
+    }
+
+    public Personal(Integer ID, String Name, String Address, String email, String no_hp,
+            Double saldo, Double KTP, String tgl_lahir, Card card) {
+        super(ID, Name, Address, email, no_hp, saldo, card);
+        this.KTP = new SimpleDoubleProperty(KTP);
+        this.tgl_lahir = new SimpleStringProperty(tgl_lahir);
+    }
