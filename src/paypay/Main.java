b1c5abf7d8f14dbbce062b2395b5d307ce48637c
Fan <62688472+byebyeblue@users.noreply.github.com>
2021-01-20 11:37:21 +0700

    Update Main.java

--- src/paypay/Main.java
+++ src/paypay/Main.java
@@ -9,3 +9,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import paypay.db.DBHelper;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("PaypayForm.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
