109c4a4182a775b8b163c4d1776a8123e5bf8049
Fan <62688472+byebyeblue@users.noreply.github.com>
2021-01-20 11:55:26 +0700

    Update PaypayController.java

--- src/paypay/PaypayController.java
+++ src/paypay/PaypayController.java
@@ -61,3 +61,99 @@ public class PaypayController implements Initializable {
 
     @FXML
     private Button btnClear;
+
+    @FXML
+    private TableView<?> tblAkun;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> ColName;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colNo_hp;
+
+    @FXML
+    private TableColumn<?, ?> colKTP;
+
+    @FXML
+    private TableColumn<?, ?> colTgl_lahir;
+
+    @FXML
+    private TableColumn<?, ?> colSaldo;
+
+    @FXML
+    private TableView<?> tblCard;
+
+    @FXML
+    private TableColumn<?, ?> colCCN;
+
+    @FXML
+    private TableColumn<?, ?> colJenis_kartu;
+
+    @FXML
+    private TableColumn<?, ?> colExp_date;
+
+    @FXML
+    private TableColumn<?, ?> colAlamat;
+
+    @FXML
+    private TableColumn<?, ?> colSec_num;
+
+    @FXML
+    private TextField tfNewID;
+
+    @FXML
+    private TextField tfNewCCN;
+
+    @FXML
+    private TextField tfNewJenis_kartu;
+
+    @FXML
+    private TextField tfNewExp_date;
+
+    @FXML
+    private TextField tfNewAlamat_penagihan;
+
+    @FXML
+    private TextField tfNewSec_num;
+
+    @FXML
+    private Button btnAddCard;
+
+    @FXML
+    private Label lblDBStatus;
+
+    @FXML
+    void handleAddAccountButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleAddCardButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleClearButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleReloadButton(ActionEvent event) {
+
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
