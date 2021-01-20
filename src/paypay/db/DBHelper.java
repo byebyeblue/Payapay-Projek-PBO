b197654c257d23f4fcb9d6e13d67d74d2f626267
Fan <62688472+byebyeblue@users.noreply.github.com>
2021-01-20 11:34:30 +0700

    Update DBHelper.java

--- src/paypay/db/DBHelper.java
+++ src/paypay/db/DBHelper.java
@@ -12,3 +12,19 @@ public class DBHelper {
     private static final String DB = "paypay";
     private static final String MYCONN = "jdbc:mysql://localhost/" + DB;
     private static final String SQCONN = "jdbc:sqlite:E:\\1 COOLYEAH\\SEMESTER 3\\PBO\\sqlliteDB\\Akun.sqlite";
+    
+    public static Connection getConnection(String driver) throws SQLException{
+        Connection conn = null;
+        switch(driver){
+            case "SQLITE":{
+                try {
+                    Class.forName("org.sqlite.JDBC");
+                    conn = DriverManager.getConnection(SQCONN);
+                } catch (ClassNotFoundException ex) {
+                    System.out.println("library tidak ada");
+                    Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                break;
+            }
+
+    
