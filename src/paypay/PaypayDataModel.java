df13ccccd3cc26ccbde856287fb93f4eeb68a089
apriladwiutami <62372893+apriladwiutami@users.noreply.github.com>
2021-01-25 23:43:46 +0700

    Update PaypayDataModel.java

--- src/paypay/PaypayDataModel.java
+++ src/paypay/PaypayDataModel.java
@@ -30,3 +30,33 @@ public class PaypayDataModel {
         stmtAkun.setString(5, holder.getNo_hp());
         stmtAkun.setDouble(6, holder.getSaldo());
         stmtAkun.execute();
+
+                
+        PreparedStatement stmtPersonal = conn.prepareStatement(insertPersonal);
+        stmtPersonal.setInt(1, holder.getID());
+        stmtPersonal.setDouble(2, holder.getKTP());
+        stmtPersonal.setString(3, holder.getTgl_lahir());
+        stmtPersonal.execute();
+        
+        PreparedStatement stmtCard = conn.prepareStatement(insertCard);
+        stmtCard.setDouble(1, holder.getCards().get(0).getCCN());
+        stmtCard.setString(2, holder.getCards().get(0).getJenisKartu());
+        stmtCard.setString(3, holder.getCards().get(0).getExpDate());
+        stmtCard.setString(4, holder.getCards().get(0).getAlamat_penagihan());
+        stmtCard.setInt(5, holder.getCards().get(0).getSecNum());
+        stmtCard.setInt(6, holder.getID());
+        stmtCard.execute();
+    }
+    public void addAkun(Bisnis holder) throws SQLException{
+        String insertAkun = "INSERT INTO akun (ID, name, address, email, no_hp, saldo)" + " VALUES (?, ?, ?, ?, ?, ?)";
+        String insertBisnis = "INSERT INTO bisnis (ID, telo_bisnis, nama_bisnis, mata_uang)" + " VALUES (?, ?, ?, ?)";
+        String insertCard = "INSERT INTO card (CCN, jenis_kartu, exp_date, alamat_penagihan, sec_num, ID)" + " VALUES (?, ?, ?, ?, ?, ?)";
+        PreparedStatement stmtAkun = conn.prepareStatement(insertAkun);
+        stmtAkun.setInt(1, holder.getID());
+        stmtAkun.setString(2, holder.getName());
+        stmtAkun.setString(3, holder.getAddress());
+        stmtAkun.setString(4, holder.getEmail());
+        stmtAkun.setString(5, holder.getNo_hp());
+        stmtAkun.setDouble(6, holder.getSaldo());
+        stmtAkun.execute();
+        
