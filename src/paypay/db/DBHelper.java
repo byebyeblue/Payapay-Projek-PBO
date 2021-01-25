73eeb636795225bfa96125deb397bc32176ba286
Fan <62688472+byebyeblue@users.noreply.github.com>
2021-01-25 23:53:44 +0700

    Update DBHelper.java

--- src/paypay/db/DBHelper.java
+++ src/paypay/db/DBHelper.java
@@ -45,3 +45,45 @@ public class DBHelper {
 
         return conn;
     }
+    
+    public static void createTable(Connection conn, String driver) throws SQLException {
+        String sqlCreate = "";
+        switch (driver) {
+            case "MYSQL": {
+                sqlCreate = "CREATE TABLE IF NOT EXISTS `akun` ("
+                        + "  `ID` int(10) NOT NULL,"
+                        + "  `name` varchar(100) DEFAULT NULL,"
+                        + "  `address` varchar(200) DEFAULT NULL,"
+                        + "  `email` varchar(35) DEFAULT NULL,"
+                        + "  `no_hp` varchar(15) DEFAULT NULL,"
+                        + "  `saldo` double DEFAULT NULL,"
+                        + "  PRIMARY KEY (`ID`)"
+                        + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+                        + "CREATE TABLE IF NOT EXISTS `bisnis` ("
+                        + "  `ID` int(10) NOT NULL,"
+                        + "  `telp_bisnis` varchar(15) DEFAULT NULL,"
+                        + "  `nama_bisnis` varchar(50) DEFAULT NULL,"
+                        + "  `mata_uang` varchar(20) DEFAULT NULL,"
+                        + "  PRIMARY KEY (`ID`),"
+                        + "  FOREIGN KEY (`ID`) REFERENCES `akun` (`ID`) ON UPDATE CASCADE"
+                        + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+                        + "CREATE TABLE IF NOT EXISTS `card` ("
+                        + "  `CCN` double NOT NULL,"
+                        + "  `jenis_kartu` varchar(50) DEFAULT NULL,"
+                        + "  `exp_date` date DEFAULT NULL,"
+                        + "  `alamat_penagihan` varchar(200) DEFAULT NULL,"
+                        + "  `sec_num` int(3) DEFAULT NULL,"
+                        + "  `ID` int(10) DEFAULT NULL,"
+                        + "  PRIMARY KEY (`CCN`),"
+                        + "  KEY `ID` (`ID`),"
+                        + "  FOREIGN KEY (`ID`) REFERENCES `akun` (`ID`) ON UPDATE CASCADE"
+                        + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+                        + "CREATE TABLE IF NOT EXISTS `personal` ("
+                        + "  `ID` int(10) NOT NULL,"
+                        + "  `KTP` double DEFAULT NULL,"
+                        + "  `tgl_lahir` date DEFAULT NULL,"
+                        + "  PRIMARY KEY (`ID`),"
+                        + "  FOREIGN KEY (`ID`) REFERENCES `akun` (`ID`) ON UPDATE CASCADE"
+                        + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+                break;
+            }
