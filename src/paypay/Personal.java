2affd7f4c8703f197f1db48e76afc3eca6bbeccf
Fan <62688472+byebyeblue@users.noreply.github.com>
2021-01-25 23:50:01 +0700

    Update Personal.java

--- src/paypay/Personal.java
+++ src/paypay/Personal.java
@@ -16,35 +16,3 @@ public class Personal extends Akun {
         this.KTP = new SimpleDoubleProperty(KTP);
         this.tgl_lahir = new SimpleStringProperty(tgl_lahir);
     }
-
-    public Personal(Integer ID, String Name, String Address, String email, String no_hp,
-            Double saldo, Double KTP, String tgl_lahir, Card card) {
-        super(ID, Name, Address, email, no_hp, saldo, card);
-        this.KTP = new SimpleDoubleProperty(KTP);
-        this.tgl_lahir = new SimpleStringProperty(tgl_lahir);
-    }
-    
-    public Double getKTP() {
-        return KTP.get();
-    }
-
-    public void setKTP(Double KTP) {
-        this.KTP.set(KTP);
-    }
-
-    public String getTgl_lahir() {
-        return tgl_lahir.get();
-    }
-
-    public void setTgl_lahir(String tgl_lahir) {
-        this.tgl_lahir.set(tgl_lahir);
-    }
-    
-    public DoubleProperty KTPProperty() {
-        return KTP;
-    }
-    
-    public StringProperty Tgl_lahirProperty() {
-        return tgl_lahir;
-    }
-}
