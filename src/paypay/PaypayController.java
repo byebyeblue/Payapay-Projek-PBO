0f5cf22dc728d780d3991cada7d197edf6dc0a01
apriladwiutami <62372893+apriladwiutami@users.noreply.github.com>
2021-01-25 23:41:27 +0700

    Update PaypayController.java

--- src/paypay/PaypayController.java
+++ src/paypay/PaypayController.java
@@ -151,3 +151,116 @@ public class PaypayController implements Initializable {
         tblCard.setItems(null);
         tblCard.setItems(data);
     }
+    @FXML    
+    void handleAddAccountButton(ActionEvent event) {
+        LocalDate ld = dpTgl_lahir.getValue();
+        String Tgl_lahir = String.format("%d-%02d-%02d", ld.getYear(), 
+                ld.getMonthValue(), ld.getDayOfMonth());
+        LocalDate xd = dpExp_date.getValue();
+        String Exp_date = String.format("%d-%02d-%02d", xd.getYear(), 
+                xd.getMonthValue(), xd.getDayOfMonth());
+        Personal holder = new Personal(Integer.parseInt(tfID.getText()), 
+                tfName.getText(), tfAddress.getText(), tfEmail.getText(), 
+                tfNo_hp.getText(), Double.parseDouble(tfSaldo.getText()), 
+                Double.parseDouble(tfKTP.getText()), Tgl_lahir, 
+                new Card(Double.parseDouble(tfCCN.getText()),
+                tfJenis_kartu.getText(), Exp_date, tfAlamat_penagihan.getText(),
+                Integer.parseInt(tfSec_num.getText())));
+        
+        try {
+            ahdm.addAkun(holder);
+            lblSaveStatus.setText("Akun Berhasil Dibuat");
+            btnReload.fire();
+            btnClear.fire();
+        } catch (SQLException ex) {
+            lblSaveStatus.setText("Akun Gagal Dibuat");
+            Logger.getLogger(PaypayController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    void handleAddCardButton(ActionEvent event) {
+        LocalDate xpd = dpNewExp_date.getValue();
+        String NewExp = String.format("%d-%02d-%02d", xpd.getYear(), 
+                xpd.getMonthValue(), xpd.getDayOfMonth());
+        try {
+            ahdm.addCard(Integer.parseInt(tfNewID.getText()),
+                    new Card(Double.parseDouble(tfNewCCN.getText()),
+                            tfNewJenis_kartu.getText(),
+                            NewExp,
+                            tfNewAlamat_penagihan.getText(),
+                            Integer.parseInt(tfNewSec_num.getText())));
+            viewDataCard(Integer.parseInt(tfNewID.getText()));
+            btnReload.fire();
+            tfNewCCN.setText("");
+            tfNewJenis_kartu.setText("");
+            tfNewAlamat_penagihan.setText("");
+            tfNewSec_num.setText("");
+        } catch (SQLException ex) {
+            Logger.getLogger(PaypayController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    void handleClearButton(ActionEvent event) {
+        try {
+            tfID.setText(""+ahdm.nextID());
+            tfName.setText("");
+	    tfAddress.setText("");
+	    tfEmail.setText("");
+	    tfNo_hp.setText("");
+	    tfKTP.setText("");
+	    tfSaldo.setText("");
+	    tfCCN.setText("");
+	    tfJenis_kartu.setText("");
+	    tfAlamat_penagihan.setText("");
+	    tfSec_num.setText("");
+            
+            dpTgl_lahir.setValue(LocalDate.of(LocalDate.now().getYear() - 20, 
+                    LocalDate.now().getMonthValue(), 
+                    LocalDate.now().getDayOfMonth()));
+            dpExp_date.setValue(LocalDate.of(LocalDate.now().getYear()+5, 
+                    LocalDate.now().getMonthValue(), 
+                    LocalDate.now().getDayOfMonth()));
+        } catch (SQLException ex) {
+            Logger.getLogger(PaypayController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	 
+    }
+ 
+    @FXML
+    void handleReloadButton(ActionEvent event) {
+	ObservableList<Personal> data = ahdm.getPersonals();
+	colID.setCellValueFactory(new PropertyValueFactory<>("ID"));
+	colName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+	colAddress.setCellValueFactory(new PropertyValueFactory<>("Address"));
+	colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+	colNo_hp.setCellValueFactory(new PropertyValueFactory<>("no_hp"));
+	colKTP.setCellValueFactory(new PropertyValueFactory<>("KTP"));
+	colTgl_lahir.setCellValueFactory(new PropertyValueFactory<>("tgl_lahir"));
+	colSaldo.setCellValueFactory(new PropertyValueFactory<>("saldo"));
+	tblAkun.setItems(null);
+	tblAkun.setItems(data);
+
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            // TODO
+            ahdm = new PaypayDataModel("MYSQL");
+            lblDBStatus.setText(ahdm.conn == null ? "Not_Connected": "Connected");
+            btnClear.fire();
+            btnReload.fire();
+        } catch (SQLException ex) {
+            Logger.getLogger(PaypayController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        tblAkun.getSelectionModel().selectedIndexProperty().addListener(listener->{
+            if(tblAkun.getSelectionModel().getSelectedItem()!=null){
+                Personal holder = tblAkun.getSelectionModel().getSelectedItem();
+                viewDataCard(holder.getID());
+            }
+        });
+    }    
+    
+}
